e2a8e04115a58fafde79799ba20f6d44a78cba8f
shafasalsa1 <112202206912@mhs.dinus.ac.id>
2025-09-30 12:44:37 +0700

    Update 1-1.shallow_run_iuxray.sh low resource

--- scripts/1-1.shallow_run_iuxray.sh
+++ scripts/1-1.shallow_run_iuxray.sh
@@ -11,8 +11,8 @@ python -u train.py \
     --dataset ${dataset} \
     --annotation ${annotation} \
     --base_dir ${base_dir} \
-    --batch_size 8 \
-    --val_batch_size 12 \
+    --batch_size 2 \
+    --val_batch_size 2 \
     --freeze_vm True \
     --vis_use_lora False \
     --savedmodel_path ${savepath} \
@@ -22,7 +22,7 @@ python -u train.py \
     --repetition_penalty 2.0 \
     --length_penalty 2.0 \
     --num_workers 8 \
-    --devices 2 \
+    --devices 1 \
     --max_epochs 15 \
     --limit_val_batches 1.0 \
     --val_check_interval 1.0 \
