e9361bc348e1965004a119998817acf241449d8e
shafasalsa1 <112202206912@mhs.dinus.ac.id>
2025-10-02 19:38:09 +0700

    Update 2-1.delta_run_iuxray.sh

--- scripts/2-1.delta_run_iuxray.sh
+++ scripts/2-1.delta_run_iuxray.sh
@@ -11,19 +11,21 @@ python -u train.py \
     --dataset ${dataset} \
     --annotation ${annotation} \
     --base_dir ${base_dir} \
-    --batch_size 8 \
-    --val_batch_size 12 \
+    --batch_size 1 \
+    --val_batch_size 2 \
     --freeze_vm True \
     --vis_use_lora True \
     --savedmodel_path ${savepath} \
-    --max_length 60 \
+    --max_length 50 \
     --min_new_tokens 40 \
     --max_new_tokens 100 \
     --repetition_penalty 2.0 \
+    --precision 16-mixed \
     --length_penalty 2.0 \
-    --num_workers 8 \
-    --devices 2 \
-    --max_epochs 15 \
+    --num_workers 2 \
+    --accumulate_grad_batches=8 \
+    --devices 1 \
+    --max_epochs 1 \
     --limit_val_batches 1.0 \
     --val_check_interval 1.0 \
     --num_sanity_val_steps 2 \
