f33f1119afaeb51b67992208151b15115c326f58
shafasalsa1 <112202206912@mhs.dinus.ac.id>
2025-10-03 13:26:53 +0700

    Update 1-1.shallow_run_iuxray.sh

--- scripts/1-1.shallow_run_iuxray.sh
+++ scripts/1-1.shallow_run_iuxray.sh
@@ -25,7 +25,7 @@ python -u train.py \
     --num_workers 2 \
     --accumulate_grad_batches=8 \
     --devices 2 \
-    --max_epochs 15 \
+    --max_epochs 2 \
     --limit_val_batches 1.0 \
     --val_check_interval 1.0 \
     --num_sanity_val_steps 0 \
